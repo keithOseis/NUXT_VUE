640152d6ce6ec2533eda4f4a02701a4269e0ccd2
keithOseis <78473033+keithOseis@users.noreply.github.com>
2021-02-18 11:44:36 +0100

    push

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,21 +1,49 @@
-export default {
-  target: "static",
-  mode: "universal",
+module.exports = {
+  /*
+   ** Headers of the page
+   */
   head: {
     title: "VUE Training",
     meta: [
       { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" }
-    ]
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Nuxt.js project" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"
+      }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
-  css: [
-    // Load a Node.js module directly (here it's a Sass file)
-    "bulma",
-    // CSS file in the project
-    "~/assets/styles/main.css",
-    // SCSS file in the project
-    "~/assets/styles/main.scss"
-  ],
-  modules: [["nuxt-buefy"], "@nuxtjs/font-awesome"],
-  buefy: { defaultIconPack: "fas" }
+  plugins: ["~plugins/vuetify.js"],
+  css: ["~/assets/style/app.styl"],
+  modules: ["@nuxtjs/axios"],
+  axios: {
+    // proxyHeaders: false
+  },
+  /*
+   ** Customize the progress bar color
+   */
+  loading: { color: "#3B8070" },
+  /*
+   ** Build configuration
+   */
+  build: {
+    vendor: ["~/plugins/vuetify.js", "axios"],
+    extractCSS: true,
+    /*
+     ** Run ESLint on save
+     */
+    extend(config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: "pre",
+          test: /\.(js|vue)$/,
+          loader: "eslint-loader",
+          exclude: /(node_modules)/
+        });
+      }
+    }
+  }
 };
